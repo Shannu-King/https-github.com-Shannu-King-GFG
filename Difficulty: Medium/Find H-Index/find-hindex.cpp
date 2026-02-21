3ed95d816eb9c840890a1e08b081b9a6167dbadb
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-02-21 20:28:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int hIndex(vector<int>& c) {
+        // code here
+        sort(c.begin(),c.end());
+        reverse(c.begin(),c.end());
+        int ans=0;
+        int h=0;
+        for(int i=0;i<c.size();i++)
+        {
+            
+            if(c[i]>=i+1)
+            h=i+1;
+            else
+            break;
+        } 
+        return h;
+    }
+};
